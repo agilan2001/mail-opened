2633902cce86f735f10a8a031e9f45d95875da07
agilan2001 <agilanvlr2001@gmail.com>
2020-12-28 18:56:12 +0530

    git init

--- lambda/index.js
+++ lambda/index.js
@@ -1,11 +1,12 @@
-const {readFileSync} = require('fs');
-
-var img = readFileSync('./empty_image.png');
-
 exports.handler = async (event) => {
     const response = {
+        headers:{
+            'Content-Type':'image/png',
+            'Access-Control-Allow-Origin':'*'
+        },
         statusCode: 200,
-        body: img.toString('base64'),
+        body: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=',
+        isBase64Encoded : true
     };
     return response;
 };
