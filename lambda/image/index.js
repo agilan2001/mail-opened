02bd2156c5e7517eea1380c6625c4a4b06eef708
agilan2001 <agilanvlr2001@gmail.com>
2021-03-15 20:43:35 +0530

    change main pass

--- lambda/image/index.js
+++ lambda/image/index.js
@@ -6,7 +6,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'agilanvlr2001@gmail.com',
-        pass: 'tlbehdowmlmiqsda'
+        pass: 'jyumjymdymforksp'
     },
     tls: {
         rejectUnauthorized: false
